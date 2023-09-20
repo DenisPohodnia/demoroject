d1c9811c72b13c6452c0bcf6f1ffb33e69033887
DenisPohodnia <pohodniad@mail.ru>
2023-09-20 21:20:24 +0300

    add module strings.py

--- demoproject/demofunction.py
+++ demoproject/demofunction.py
@@ -6,6 +6,11 @@ text = "new text"
 def minus(a,b):
     return a-b
 
+def multi(a,b):
+    return a*b
 
 if __name__ == "__main__":
     print(demolol(10,20))
+    print(minus(10, 20))
+    print(multi(10, 20))
+
